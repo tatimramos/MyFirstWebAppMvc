6f903367f089b0a36f239bfa62124da5477f96ec
Tatiane Ramos <tatiane.mramos@gmail.com>
2023-07-05 17:45:22 -0300

    CRUD Scaffolding

--- WebAppSales/Program.cs
+++ WebAppSales/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebAppSales.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<WebAppSalesContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebAppSalesContext") ?? throw new InvalidOperationException("Connection string 'WebAppSalesContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
