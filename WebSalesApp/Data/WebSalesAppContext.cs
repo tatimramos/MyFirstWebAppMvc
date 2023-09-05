52cfe9648faa23ba9be1d0220f5082ee61157028
unknown <tatiane.mramos@gmail.com>
2023-09-05 14:49:06 -0300

    Project MVC DB records

---
+++ WebSalesApp/Data/WebSalesAppContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebSalesApp.Models;
+
+namespace WebSalesApp.Data
+{
+    public class WebSalesAppContext : DbContext
+    {
+        public WebSalesAppContext (DbContextOptions<WebSalesAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Department> Department { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+    }
+}
---
+++ WebSalesApp/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebSalesApp.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
